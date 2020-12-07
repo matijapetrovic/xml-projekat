4b31414e1109e987d763a03ddbfae23775b35ec0
Matija Aleksic <matijaaleksic97@gmail.com>
2020-12-07 21:36:07 +0100

    models and marshalling for zalbanaodluku done

---
+++ poc/src/main/java/rs/uns/ftn/xml/tim11/poc/zalbanaodlukucir/model/ZAdresa.java
@@ -0,0 +1,137 @@
+
+package rs.uns.ftn.xml.tim11.poc.zalbanaodlukucir.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ZAdresa complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ZAdresa"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="mesto"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;minLength value="2"/&gt;
+ *               &lt;maxLength value="200"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="ulica"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;minLength value="2"/&gt;
+ *               &lt;maxLength value="200"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="broj"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}positiveInteger"&gt;
+ *               &lt;minInclusive value="1"/&gt;
+ *               &lt;maxInclusive value="200"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ZAdresa", namespace = "zalbaO", propOrder = {
+    "mesto",
+    "ulica",
+    "broj"
+})
+public class ZAdresa {
+
+    @XmlElement(namespace = "zalbaO", required = true)
+    protected String mesto;
+    @XmlElement(namespace = "zalbaO", required = true)
+    protected String ulica;
+    @XmlElement(namespace = "zalbaO")
+    protected int broj;
+
+    /**
+     * Gets the value of the mesto property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMesto() {
+        return mesto;
+    }
+
+    /**
+     * Sets the value of the mesto property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMesto(String value) {
+        this.mesto = value;
+    }
+
+    /**
+     * Gets the value of the ulica property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUlica() {
+        return ulica;
+    }
+
+    /**
+     * Sets the value of the ulica property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUlica(String value) {
+        this.ulica = value;
+    }
+
+    /**
+     * Gets the value of the broj property.
+     * 
+     */
+    public int getBroj() {
+        return broj;
+    }
+
+    /**
+     * Sets the value of the broj property.
+     * 
+     */
+    public void setBroj(int value) {
+        this.broj = value;
+    }
+    
+    @Override
+    public String toString() {
+        return  "\t\tMesto: " + getMesto() +
+                "\n\t\t\tUlica: " + getUlica() +
+                "\n\t\t\tBroj: " + getBroj();
+    }
+}
---
+++ poc/src/main/java/rs/uns/ftn/xml/tim11/poc/zalbanaodlukucir/model/ZOsoba.java
@@ -0,0 +1,110 @@
+
+package rs.uns.ftn.xml.tim11.poc.zalbanaodlukucir.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for ZOsoba complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ZOsoba"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="ime"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;minLength value="1"/&gt;
+ *               &lt;maxLength value="100"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="prezime"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;minLength value="1"/&gt;
+ *               &lt;maxLength value="100"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ZOsoba", namespace = "zalbaO", propOrder = {
+    "ime",
+    "prezime"
+})
+public class ZOsoba {
+
+    @XmlElement(namespace = "zalbaO", required = true)
+    protected String ime;
+    @XmlElement(namespace = "zalbaO", required = true)
+    protected String prezime;
+
+    /**
+     * Gets the value of the ime property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIme() {
+        return ime;
+    }
+
+    /**
+     * Sets the value of the ime property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIme(String value) {
+        this.ime = value;
+    }
+
+    /**
+     * Gets the value of the prezime property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPrezime() {
+        return prezime;
+    }
+
+    /**
+     * Sets the value of the prezime property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPrezime(String value) {
+        this.prezime = value;
+    }
+    
+    @Override
+    public String toString() {
+        return "\t\tIme: " + getIme() +
+                "\n\t\t\tPrezime: " + getPrezime();
+    }
+
+}
