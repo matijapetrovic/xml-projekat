07a866837421a84015a701d6d3a13a79cc70439b
nikolakabasaj <44526113+nikolakabasaj@users.noreply.github.com>
2021-02-06 12:11:21 +0100

    :)

--- web-apps/projects/authority-body/src/app/modules/zahtev/zahtev.module.ts
+++ web-apps/projects/authority-body/src/app/modules/zahtev/zahtev.module.ts
@@ -4,14 +4,14 @@ import { AddZahtevComponent } from './components/add-zahtev/add-zahtev.component
 import { ZahtevRoutingModule } from './zahtev-routing.module';
 import { XonomyModule } from 'projects/xonomy/src/public-api';
 import { VendorsModule } from 'projects/vendors/src/projects';
-import { ZahteviViewComponent } from './components/zahtevi-view/zahtevi-view.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from  '../../shared/shared.module';
-import { ZahtevViewComponent } from './components/zahtev-view/zahtev-view.component';
 import { UserZahteviViewComponent } from './components/user-zahtevi-view/user-zahtevi-view.component';
-import { ZahtevXhtmlViewComponent } from './components/zahtev-xhtml-view/zahtev-xhtml-view.component';
 import { ZahtevPdfViewComponent } from './components/zahtev-pdf-view/zahtev-pdf-view.component';
+import { ZahtevViewComponent } from './components/zahtev-view/zahtev-view.component';
+import { ZahtevXhtmlViewComponent } from './components/zahtev-xhtml-view/zahtev-xhtml-view.component';
+import { ZahteviViewComponent } from './components/zahtevi-view/zahtevi-view.component';
 
 @NgModule({
   declarations: [AddZahtevComponent, ZahteviViewComponent, ZahtevViewComponent, UserZahteviViewComponent, ZahtevXhtmlViewComponent, ZahtevPdfViewComponent],
@@ -22,7 +22,7 @@ import { ZahtevPdfViewComponent } from './components/zahtev-pdf-view/zahtev-pdf-
     ZahtevRoutingModule,
     FlexLayoutModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
   ]
 })
 export class ZahtevModule { }
