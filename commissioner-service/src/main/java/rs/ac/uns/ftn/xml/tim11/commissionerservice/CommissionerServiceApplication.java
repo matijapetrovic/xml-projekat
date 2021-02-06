09c66cca1ad1a6d7bc5e57569ee54c536a25d47c
nikolakabasaj <44526113+nikolakabasaj@users.noreply.github.com>
2021-02-06 10:39:30 +0100

    Bugs fixed

--- commissioner-service/src/main/java/rs/ac/uns/ftn/xml/tim11/commissionerservice/CommissionerServiceApplication.java
+++ commissioner-service/src/main/java/rs/ac/uns/ftn/xml/tim11/commissionerservice/CommissionerServiceApplication.java
@@ -9,15 +9,20 @@ import org.xmldb.api.base.XMLDBException;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.model.user.Account;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.model.user.Authority;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.model.user.User;
+import rs.ac.uns.ftn.xml.tim11.commissionerservice.model.zalbacutanje.ZalbaCutanje;
+import rs.ac.uns.ftn.xml.tim11.commissionerservice.repository.rdf.ZalbaCutanjeRDFRepository;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.repository.xml.AccountXmlRepository;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.repository.xml.AuthorityXmlRepository;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.repository.xml.UserXmlRepository;
+import rs.ac.uns.ftn.xml.tim11.commissionerservice.repository.xml.ZalbaCutanjeXmlRepository;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.util.AccountProperties;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.util.AuthorityProperties;
 import rs.ac.uns.ftn.xml.tim11.commissionerservice.util.UserProperties;
+import rs.ac.uns.ftn.xml.tim11.commissionerservice.util.ZalbaCutanjeProperties;
 import rs.ac.uns.ftn.xml.tim11.xmllib.jaxb.JaxbMarshaller;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.transform.TransformerException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -47,7 +52,23 @@ public class CommissionerServiceApplication {
 //			testResenje(resenjeXmlRepository, resenjeRDFRepository, resenjeProperties);
 //		};
 //	}
-//
+
+	@Bean
+	public CommandLineRunner run(
+			AuthorityXmlRepository authorityXmlRepository,
+			AuthorityProperties authorityProperties,
+			AccountXmlRepository accountXmlRepository,
+			AccountProperties accountProperties,
+			UserXmlRepository userXmlRepository,
+			UserProperties userProperties) {
+		return args -> {
+			insertAuthority(authorityXmlRepository, authorityProperties);
+			insertAccount(accountXmlRepository, accountProperties);
+			insertUser(userXmlRepository, userProperties);
+		};
+	}
+
+
 //	public void testZalbaCutanje(ZalbaCutanjeXmlRepository xmlRepository, ZalbaCutanjeRDFRepository rdfRepository, ZalbaCutanjeProperties properties) throws JAXBException, SAXException, FileNotFoundException, TransformerException, XMLDBException {
 //		JaxbMarshaller<ZalbaCutanje> m = new JaxbMarshaller<>(properties);
 //		ZalbaCutanje resenje = m.unmarshal(new FileInputStream("data/xml/zalbacutanje1.xml"));
@@ -57,7 +78,6 @@ public class CommissionerServiceApplication {
 //
 //		rdfRepository.saveMetadata(resenje);
 //	}
-//
 //	public void testZalbaNaOdluku(ZalbaNaOdlukuXmlRepository xmlRepository, ZalbaNaOdlukuRDFRepository rdfRepository, ZalbaNaOdlukuProperties properties) throws JAXBException, SAXException, FileNotFoundException, TransformerException, XMLDBException {
 //		JaxbMarshaller<ZalbaNaOdluku> m = new JaxbMarshaller<>(properties);
 //		ZalbaNaOdluku resenje = m.unmarshal(new FileInputStream("data/xml/zalbanaodluku1.xml"));
@@ -78,30 +98,30 @@ public class CommissionerServiceApplication {
 //		rdfRepository.saveMetadata(resenje);
 //	}
 
-//	public void insertAuthority(AuthorityXmlRepository xmlRepository, AuthorityProperties properties) throws JAXBException, SAXException, IOException, XMLDBException {
-//		JaxbMarshaller<Authority> m = new JaxbMarshaller<>(properties);
-//
-//		Authority authority = m.unmarshal(new FileInputStream("data/xml/authority.xml"));
-//
-//		Long id = xmlRepository.createWithId(authority, 1L);
-//		authority = xmlRepository.findById(id).get();
-//	}
-//
-//	public void insertAccount(AccountXmlRepository xmlRepository, AccountProperties properties) throws JAXBException, XMLDBException, FileNotFoundException, SAXException {
-//		JaxbMarshaller<Account> m = new JaxbMarshaller<>(properties);
-//
-//		Account authority = m.unmarshal(new FileInputStream("data/xml/account.xml"));
-//
-//		Long id = xmlRepository.createWithId(authority, 1L);
-//		authority = xmlRepository.findById(id).get();
-//	}
-//
-//	public void insertUser(UserXmlRepository xmlRepository, UserProperties properties) throws FileNotFoundException, JAXBException, XMLDBException, SAXException {
-//		JaxbMarshaller<User> m = new JaxbMarshaller<>(properties);
-//
-//		User authority = m.unmarshal(new FileInputStream("data/xml/user.xml"));
-//
-//		Long id = xmlRepository.createWithId(authority, 1L);
-//		authority = xmlRepository.findById(id).get();
-//	}
+	public void insertAuthority(AuthorityXmlRepository xmlRepository, AuthorityProperties properties) throws JAXBException, SAXException, IOException, XMLDBException {
+		JaxbMarshaller<Authority> m = new JaxbMarshaller<>(properties);
+
+		Authority authority = m.unmarshal(new FileInputStream("data/xml/authority.xml"));
+
+		Long id = xmlRepository.createWithId(authority, 1L);
+		authority = xmlRepository.findById(id).get();
+	}
+
+	public void insertAccount(AccountXmlRepository xmlRepository, AccountProperties properties) throws JAXBException, XMLDBException, FileNotFoundException, SAXException {
+		JaxbMarshaller<Account> m = new JaxbMarshaller<>(properties);
+
+		Account authority = m.unmarshal(new FileInputStream("data/xml/account.xml"));
+
+		Long id = xmlRepository.createWithId(authority, 1L);
+		authority = xmlRepository.findById(id).get();
+	}
+
+	public void insertUser(UserXmlRepository xmlRepository, UserProperties properties) throws FileNotFoundException, JAXBException, XMLDBException, SAXException {
+		JaxbMarshaller<User> m = new JaxbMarshaller<>(properties);
+
+		User authority = m.unmarshal(new FileInputStream("data/xml/user.xml"));
+
+		Long id = xmlRepository.createWithId(authority, 1L);
+		authority = xmlRepository.findById(id).get();
+	}
 }
